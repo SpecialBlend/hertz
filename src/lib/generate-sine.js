adc8ed1de33bf973bab408a8c86c1b9dcbbd66d2
CJ Salem <cj.salem@fishtech.group>
2018-12-08 19:03:15 -0600

    generate sine wave for mock data

---
+++ src/lib/generate-sine.js
@@ -0,0 +1,14 @@
+/**
+ * Generate a sine waveform
+ * @param {Number} size - wave size in bytes (e.g. 1024)
+ * @param {Number} sampleRate - wave sample rate in Hz (e.g. 44100)
+ * @param {Number} frequency - wave frequency in Hz (e.g. 60)
+ * @returns {Float32Array} - wave sample
+ */
+export const generateSine = (size, sampleRate, frequency) => {
+    const wave = new Float32Array(size);
+    for (let i = 0; i < size; i++) {
+        wave[i] = Math.sin(i / sampleRate * Math.PI * 2 * frequency);
+    }
+    return wave;
+};
